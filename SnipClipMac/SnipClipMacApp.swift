04b2846aca4a2842c850b4c00b14b3a52c28ff93
Andante <li191380469@gmail.com>
2026-06-13 18:40:07 +0800

    Initial Commit

---
+++ SnipClipMac/SnipClipMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnipClipMacApp.swift
+//  SnipClipMac
+//
+//  Created by li on 2026/6/13.
+//
+
+import SwiftUI
+
+@main
+struct SnipClipMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
